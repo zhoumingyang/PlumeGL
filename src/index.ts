c910452a2ae2044e6a247f7cc62eb3dfda585756
MingYang <yang@MingYang-iMac.local>
2019-07-19 09:32:10 +0800

    add fbo multisample example

--- src/index.ts
+++ src/index.ts
@@ -6,6 +6,7 @@ import { FboNewBlenEquation } from './example/fbo_new_blend_equation';
 import { FboRttDrawBuffers } from './example/fbo_rtt_draw_buffers';
 import { DrawLine } from './example/draw_line';
 import { DrawPoints } from './example/draw_points';
+import { FboMultiSample } from './example/fbo_multisample'
 
 const appRun = () => {
     // SampleObject();
@@ -13,8 +14,9 @@ const appRun = () => {
     // BufferUniform();
     // DrawInstancedUbo();
     // FboNewBlenEquation();
-    FboRttDrawBuffers();
+    // FboRttDrawBuffers();
     // DrawLine();
     // DrawPoints();
+    FboMultiSample();
 };
 appRun();
