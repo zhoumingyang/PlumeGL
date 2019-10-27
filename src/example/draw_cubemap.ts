b5c6b18f934c4fdbc38788f985798e199d56ecea
zhoumingyoung <1192630079@qq.com>
2019-10-27 21:51:30 +0800

    tmp commit

--- src/example/draw_cubemap.ts
+++ src/example/draw_cubemap.ts
@@ -20,6 +20,8 @@ export const DrawCubeMap = () => {
 
     const defaultCubeMap = new PlumeGL.DefaultCubeMapShader();
     defaultCubeMap.initParameters();
+    const defaultColor = new PlumeGL.DefaultColorShader();
+    defaultColor.initParameters();
 
     const imgUrls: string[] = [
         '../../res/cubemap/right.jpg', '../../res/cubemap/left.jpg',
@@ -30,13 +32,16 @@ export const DrawCubeMap = () => {
     cubeGeometry.create(200, 200, 200);
     const mesh = new PlumeGL.Mesh();
     mesh.setGeometryAttribute(cubeGeometry.vertices, defaultCubeMap.positionAttribute, gl.STATIC_DRAW, 3, gl.FLOAT, false);
+    mesh.initBufferAttributePoint(defaultColor);
 
     const cubTex = new PlumeGL.TextureCube();
     cubTex.setFormat(gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE);
 
     const p3d = new PlumeGL.P3D(mesh, cubTex);
     defaultCubeMap.addDrawObject(p3d);
+    // defaultColor.addDrawObject(p3d);
     p3d.setSelfUniform('uOpacity', [1.0]);
+    // p3d.setSelfUniform('uColor', [1.0, 0.0, 0.0])
     p3d.setSelfUniform('uFlip', [1.0]);
     p3d.setSelfUniform('uToneMappingExposure', [1.0]);
 
@@ -48,23 +53,27 @@ export const DrawCubeMap = () => {
     camera.setPersective(fieldOfView, aspect, zNear, zFar);
     camera.setView(
         new PlumeGL.Vec3(0.0, 0.0, 0.0),
-        new PlumeGL.Vec3(0.0, 0.0, -1000.0),
+        new PlumeGL.Vec3(0.0, 0.0, 1000.0),
         new PlumeGL.Vec3(0.0, 1.0, 0.0));
     camera.updateMat();
 
     const scene = new PlumeGL.Scene();
     scene.add(defaultCubeMap);
+    // scene.add(defaultColor);
     scene.setSceneState(new PlumeGL.State());
     scene.state.setClearColor(0.0, 0.0, 0.0, 1.0);
     scene.state.setClear(true, false, false);
     scene.state.setDepthTest(true);
+    // scene.state.setCullFace(true, PlumeGL.STATE.CULLFACE_FRONT);
+    // scene.state.setCullFace(true, PlumeGL.STATE.CULLFACE_BACK);
+    // scene.state.setCullFace(true, PlumeGL.STATE.CULLFACE_FRONT_AND_BACK);
     scene.setActiveCamera(camera);
 
     function render() {
         scene.stateChange();
         scene.forEachRender((shaderObj: any) => {
             const projectMat = camera.getProjectMat();
-            if (shaderObj === PlumeGL.CONSTANT.DEFAULTCUBEMAPSHADER) {
+            if (shaderObj.type === PlumeGL.CONSTANT.DEFAULTCUBEMAPSHADER) {
                 shaderObj.forEachDraw((obj: any) => {
                     const modelMat = obj.getModelMat();
                     const modelViewMat = camera.getModelViewMat(modelMat);
@@ -74,6 +83,17 @@ export const DrawCubeMap = () => {
                     obj.prepare();
                     obj.draw({ start: 0, cnt: 36 });
                     obj.unPrepare();
+                });
+            }
+            if (shaderObj.type === PlumeGL.CONSTANT.DEFAULTCOLORSHADER) {
+                shaderObj.forEachDraw((obj: any) => {
+                    // const modelMat = obj.getModelMat();
+                    // const modelViewMat = camera.getModelViewMat(modelMat);
+                    // shaderObj.setUniformData(shaderObj.uniform.modelViewMatrix, [modelViewMat.value, false]);
+                    // shaderObj.setUniformData(shaderObj.uniform.projectionMatrix, [projectMat.value, false]);
+                    // obj.prepare();
+                    // obj.draw({ start: 0, cnt: 36 });
+                    // obj.unPrepare();
                 });
             }
         });
@@ -96,6 +116,7 @@ export const DrawCubeMap = () => {
                     for (let j = 0; j < cnt; j++) {
                         cubTex.setTextureFromImage(img[i], j);
                         cubTex.wrapMode(true);
+                        cubTex.filterMode(true, false, false);
                     }
                     cubTex.mipmap();
                     requestAnimationFrame(render);
