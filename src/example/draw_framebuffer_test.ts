1bd5df3465de2dafce4cad1c3f8faecd496add97
zhoumingyang <zmy225505@alibaba-inc.com>
2019-11-13 20:52:02 +0800

    fix frambufer test

--- src/example/draw_framebuffer_test.ts
+++ src/example/draw_framebuffer_test.ts
@@ -2,8 +2,104 @@ import { PlumeGL } from '../plumegl/engine/plumegl';
 import { DrawFrameBufferVert } from './shader/draw_framebuffer_vert';
 import { DrawFrameBufferFrag } from './shader/draw_framebuffer_frag';
 
+const cubePositions: number[] = [
+    -0.5, -0.5, -0.5,
+    -0.5, 0.5, -0.5,
+    0.5, -0.5, -0.5,
+    -0.5, 0.5, -0.5,
+    0.5, 0.5, -0.5,
+    0.5, -0.5, -0.5,
+
+    -0.5, -0.5, 0.5,
+    0.5, -0.5, 0.5,
+    -0.5, 0.5, 0.5,
+    -0.5, 0.5, 0.5,
+    0.5, -0.5, 0.5,
+    0.5, 0.5, 0.5,
+
+    -0.5, 0.5, -0.5,
+    -0.5, 0.5, 0.5,
+    0.5, 0.5, -0.5,
+    -0.5, 0.5, 0.5,
+    0.5, 0.5, 0.5,
+    0.5, 0.5, -0.5,
+
+    -0.5, -0.5, -0.5,
+    0.5, -0.5, -0.5,
+    -0.5, -0.5, 0.5,
+    -0.5, -0.5, 0.5,
+    0.5, -0.5, -0.5,
+    0.5, -0.5, 0.5,
+
+    -0.5, -0.5, -0.5,
+    -0.5, -0.5, 0.5,
+    -0.5, 0.5, -0.5,
+    -0.5, -0.5, 0.5,
+    -0.5, 0.5, 0.5,
+    -0.5, 0.5, -0.5,
+
+    0.5, -0.5, -0.5,
+    0.5, 0.5, -0.5,
+    0.5, -0.5, 0.5,
+    0.5, -0.5, 0.5,
+    0.5, 0.5, -0.5,
+    0.5, 0.5, 0.5,
+];
+
+const cubeTexcoords: number[] = [
+    0, 0,
+    0, 1,
+    1, 0,
+    0, 1,
+    1, 1,
+    1, 0,
+
+    0, 0,
+    0, 1,
+    1, 0,
+    1, 0,
+    0, 1,
+    1, 1,
+
+    0, 0,
+    0, 1,
+    1, 0,
+    0, 1,
+    1, 1,
+    1, 0,
+
+    0, 0,
+    0, 1,
+    1, 0,
+    1, 0,
+    0, 1,
+    1, 1,
+
+    0, 0,
+    0, 1,
+    1, 0,
+    0, 1,
+    1, 1,
+    1, 0,
+
+    0, 0,
+    0, 1,
+    1, 0,
+    1, 0,
+    0, 1,
+    1, 1,
+]
+
 let cav: any;
 let gl: any;
+let last: number = 0;
+let xRad: number = 0;
+let yRad: number = 0;
+let fbo: any;
+let scene: any;
+let srcTexture: any;
+let tagTexture: any;
+
 const createGLContext = () => {
     cav = document.getElementById('main-canvas');
     cav.width = 398;
@@ -15,290 +111,139 @@ const createGLContext = () => {
     return gl;
 };
 
+const createSourceTexture = (): any => {
+    const sourceTexture = new PlumeGL.Texture2D();
+    sourceTexture.setFormat(gl.LUMINANCE, gl.LUMINANCE, gl.UNSIGNED_BYTE);
+    const data = new Uint8Array([
+        128, 64, 128,
+        0, 192, 0
+    ]);
+    PlumeGL.Texture2D.setPixelStorei(gl.UNPACK_ALIGNMENT, 1);
+    sourceTexture.setTextureFromData(data, [3, 2]);
+    sourceTexture.setFilterMode(gl.NEAREST, gl.NEAREST);
+    sourceTexture.setWrapMode(gl.CLAMP_TO_EDGE);
+    return sourceTexture;
+};
+
+const createTargetTexture = (): any => {
+    const targetTexture = new PlumeGL.Texture2D();
+    targetTexture.setFormat(gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE);
+    const data: any = null;
+    targetTexture.setTextureFromData(data, [256, 256]);
+    targetTexture.setFilterMode(gl.LINEAR, gl.LINEAR);
+    targetTexture.setWrapMode(gl.CLAMP_TO_EDGE);
+    return targetTexture;
+};
+
+const initScene = (): any => {
+    const scene = new PlumeGL.Scene();
+    scene.setSceneState(new PlumeGL.State());
+    scene.state.setClear(true, true, false);
+    scene.state.setDepthTest(true);
+    scene.state.setCullFace(true, PlumeGL.STATE.CULLFACE_BACK);
+    return scene;
+};
+
+const createFbo = (attachTexture: any) => {
+    const fb = new PlumeGL.FrameBuffer();
+    fb.attachTexture(attachTexture, gl.COLOR_ATTACHMENT0);
+    return fb;
+}
+
+const getCameraProjectViewMat = (aspect: number): any => {
+
+    function degToRad(d: number) {
+        return d * Math.PI / 180;
+    }
+    let fieldOfViewRadians = degToRad(60);
+
+    const camera = new PlumeGL.PerspectiveCamera();
+    camera.setPersective(fieldOfViewRadians, aspect, 1, 2000);
+    camera.setView(new PlumeGL.Vec3(0, 0, 2), new PlumeGL.Vec3(0, 0, 0), new PlumeGL.Vec3(0, 1, 0));
+    camera.updateMat();
+
+    const vm = camera.getViewMat();
+    vm.value[12] = Math.abs(vm.value[12]);
+    vm.value[13] = Math.abs(vm.value[13]);
+    vm.value[14] = Math.abs(vm.value[14]);
+    const ivm = vm.clone().invert();
+
+    const pm = camera.getProjectMat();
+    const pvm = pm.clone().multiply(ivm);
+
+    return pvm;
+}
+
+const draw = (aspect: number, blue: boolean) => {
+
+    const pvm = getCameraProjectViewMat(aspect);
+    let yrm = new PlumeGL.Mat4();
+    yrm = yrm.rotate(yRad, new PlumeGL.Vec3(0, 1, 0));
+    const fm = pvm.clone().multiply(yrm);
+
+    blue ? gl.clearColor(0.2, 0.2, 0.65, 1) : gl.clearColor(0, 0, 0, 1);
+    scene.state.stateChange();
+    scene.forEachRender((shaderObj: any) => {
+        shaderObj.setUniformData('uMvp', [fm.value, false]);
+        shaderObj.setUniformData('uTexture', [0]);
+        shaderObj.forEachDraw((obj: any) => {
+            obj.prepare();
+            obj.draw({ start: 0, cnt: 36 });
+            obj.unPrepare();
+        });
+    });
+};
+
+const renderScene = (time: number) => {
+
+    time *= 0.001;
+    let deltaTime = time - last;
+    last = time;
+
+    xRad += -0.7 * deltaTime;
+    yRad += -0.4 * deltaTime;
+
+    //pass1
+    let aspect: number = 256 / 256;
+    fbo.bind();
+    srcTexture.bind();
+    scene.state.setViewPort(0, 0, 256, 256);
+    draw(aspect, true);
+
+    //pass2
+    tagTexture.bind();
+    PlumeGL.FrameBuffer.unBind();
+    scene.state.setViewPort(0, 0, gl.canvas.clientWidth, gl.canvas.clientHeight);
+    aspect = gl.canvas.clientWidth / gl.canvas.clientHeight;
+    draw(aspect, false);
+
+    requestAnimationFrame(renderScene);
+}
+
 export const DrawFrameBufferTest = () => {
     gl = createGLContext();
     if (!gl) {
         return;
     }
 
+    scene = initScene();
+
     const shader = new PlumeGL.Shader(DrawFrameBufferVert, DrawFrameBufferFrag);
     shader.initParameters();
+    scene.add(shader);
 
-    const positionBuffer = gl.createBuffer();
-    gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
-    setGeometry(gl);
+    const mesh = new PlumeGL.Mesh();
+    mesh.setGeometryAttribute(new Float32Array(cubePositions), 'aPosition', gl.STATIC_DRAW, 3, gl.FLOAT, false);
+    mesh.setGeometryAttribute(new Float32Array(cubeTexcoords), 'aUv', gl.STATIC_DRAW, 2, gl.FLOAT, false);
+    mesh.initBufferAttributePoint(shader);
 
-    const texcoordBuffer = gl.createBuffer();
-    gl.bindBuffer(gl.ARRAY_BUFFER, texcoordBuffer);
-    setTexcoords(gl);
+    srcTexture = createSourceTexture();
+    tagTexture = createTargetTexture();
 
-    const texture = gl.createTexture();
-    gl.bindTexture(gl.TEXTURE_2D, texture);
-    {
-        const level = 0;
-        const internalFormat = gl.LUMINANCE;
-        const width = 3;
-        const height = 2;
-        const border = 0;
-        const format = gl.LUMINANCE;
-        const type = gl.UNSIGNED_BYTE;
-        const data = new Uint8Array([
-            128, 64, 128,
-            0, 192, 0,
-        ]);
-        const alignment = 1;
-        gl.pixelStorei(gl.UNPACK_ALIGNMENT, alignment);
-        gl.texImage2D(gl.TEXTURE_2D, level, internalFormat, width, height, border,
-            format, type, data);
-        // set the filtering so we don't need mips and it's not filtered
-        gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.NEAREST);
-        gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.NEAREST);
-        gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
-        gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
-    }
+    const p3d = new PlumeGL.P3D(mesh);
+    shader.addDrawObject(p3d);
 
-    const targetTextureWidth = 256;
-    const targetTextureHeight = 256;
-    const targetTexture = gl.createTexture();
-    gl.bindTexture(gl.TEXTURE_2D, targetTexture);
-    {
-        // define size and format of level 0
-        const level = 0;
-        const internalFormat = gl.RGBA;
-        const border = 0;
-        const format = gl.RGBA;
-        const type = gl.UNSIGNED_BYTE;
-        const data: any = null;
-        gl.texImage2D(gl.TEXTURE_2D, level, internalFormat,
-            targetTextureWidth, targetTextureHeight, border,
-            format, type, data);
+    fbo = createFbo(tagTexture);
 
-        gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR);
-        gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
-        gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
-    }
-
-    const fb = gl.createFramebuffer();
-    gl.bindFramebuffer(gl.FRAMEBUFFER, fb);
-
-    // attach the texture as the first color attachment
-    const attachmentPoint = gl.COLOR_ATTACHMENT0;
-    const level = 0;
-    gl.framebufferTexture2D(gl.FRAMEBUFFER, attachmentPoint, gl.TEXTURE_2D, targetTexture, level);
-
-    function degToRad(d: number) {
-        return d * Math.PI / 180;
-    }
-
-    let fieldOfViewRadians = degToRad(60);
-    let modelXRotationRadians = degToRad(0);
-    let modelYRotationRadians = degToRad(0);
-
-    // Get the starting time.
-    let then = 0;
-
-    requestAnimationFrame(drawScene);
-
-    function drawCube(aspect: number) {
-        shader.use();
-        gl.enableVertexAttribArray(0);
-        gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
-        var size = 3;          // 3 components per iteration
-        var type = gl.FLOAT;   // the data is 32bit floats
-        var normalize = false; // don't normalize the data
-        var stride = 0;        // 0 = move forward size * sizeof(type) each iteration to get the next position
-        var offset = 0;        // start at the beginning of the buffer
-        gl.vertexAttribPointer(0, size, type, normalize, stride, offset);
-
-        gl.enableVertexAttribArray(1);
-        gl.bindBuffer(gl.ARRAY_BUFFER, texcoordBuffer);
-        var size = 2;          // 2 components per iteration
-        var type = gl.FLOAT;   // the data is 32bit floats
-        var normalize = false; // don't normalize the data
-        var stride = 0;        // 0 = move forward size * sizeof(type) each iteration to get the next position
-        var offset = 0;        // start at the beginning of the buffer
-        gl.vertexAttribPointer(1, size, type, normalize, stride, offset);
-
-        const camera = new PlumeGL.PerspectiveCamera();
-        camera.setPersective(fieldOfViewRadians, aspect, 1, 2000);
-        camera.setView(new PlumeGL.Vec3(0, 0, 2), new PlumeGL.Vec3(0, 0, 0), new PlumeGL.Vec3(0, 1, 0));
-        camera.updateMat();
-
-        const vm = camera.getViewMat();
-        vm.value[12] = Math.abs(vm.value[12]);
-        vm.value[13] = Math.abs(vm.value[13]);
-        vm.value[14] = Math.abs(vm.value[14]);
-        const ivm = vm.clone().invert();
-
-        const pm = camera.getProjectMat();
-        const pvm = pm.clone().multiply(ivm);
-
-        // const xrm = new PlumeGL.Mat4().setRotation(modelXRotationRadians, new PlumeGL.Vec3(1, 0, 0));
-        const xrm = new PlumeGL.Mat4();
-        let yrm = new PlumeGL.Mat4();//.setRotation(modelYRotationRadians, new PlumeGL.Vec3(0, 1, 0));
-        yrm = yrm.rotate(modelYRotationRadians,new PlumeGL.Vec3(0, 1, 0));
-        const mm = yrm.clone().multiply(xrm);
-
-        const fm = pvm.clone().multiply(mm);
-
-        shader.setUniformData('uMvp', [fm.value, false]);
-        shader.setUniformData('uTexture', [0]);
-
-        gl.drawArrays(gl.TRIANGLES, 0, 6 * 6);
-    }
-
-    function drawScene(time: number) {
-        time *= 0.001;
-        // Subtract the previous time from the current time
-        var deltaTime = time - then;
-        // Remember the current time for the next frame.
-        then = time;
-
-        // Animate the rotation
-        modelYRotationRadians += -0.7 * deltaTime;
-        modelXRotationRadians += -0.4 * deltaTime;
-
-        gl.enable(gl.CULL_FACE);
-        gl.enable(gl.DEPTH_TEST);
-
-        {
-            // render to our targetTexture by binding the framebuffer
-            gl.bindFramebuffer(gl.FRAMEBUFFER, fb);
-
-            // render cube with our 3x2 texture
-            gl.bindTexture(gl.TEXTURE_2D, texture);
-
-            // Tell WebGL how to convert from clip space to pixels
-            gl.viewport(0, 0, targetTextureWidth, targetTextureHeight);
-
-            // Clear the attachment(s).
-            gl.clearColor(0, 0, 1, 1);   // clear to blue
-            gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
-
-            const aspect = targetTextureWidth / targetTextureHeight;
-            drawCube(aspect);
-        }
-
-        {
-            // render to the canvas
-            gl.bindFramebuffer(gl.FRAMEBUFFER, null);
-
-            // render the cube with the texture we just rendered to
-            gl.bindTexture(gl.TEXTURE_2D, targetTexture);
-
-            // Tell WebGL how to convert from clip space to pixels
-            gl.viewport(0, 0, gl.canvas.width, gl.canvas.height);
-
-            // Clear the canvas AND the depth buffer.
-            gl.clearColor(0, 0, 0, 1);   // clear to white
-            gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
-
-
-            const aspect = gl.canvas.clientWidth / gl.canvas.clientHeight;
-            drawCube(aspect);
-        }
-
-        requestAnimationFrame(drawScene);
-    }
-
+    requestAnimationFrame(renderScene);
 };
-
-const setGeometry = (gl: any) => {
-    var positions = new Float32Array(
-        [
-            -0.5, -0.5, -0.5,
-            -0.5, 0.5, -0.5,
-            0.5, -0.5, -0.5,
-            -0.5, 0.5, -0.5,
-            0.5, 0.5, -0.5,
-            0.5, -0.5, -0.5,
-
-            -0.5, -0.5, 0.5,
-            0.5, -0.5, 0.5,
-            -0.5, 0.5, 0.5,
-            -0.5, 0.5, 0.5,
-            0.5, -0.5, 0.5,
-            0.5, 0.5, 0.5,
-
-            -0.5, 0.5, -0.5,
-            -0.5, 0.5, 0.5,
-            0.5, 0.5, -0.5,
-            -0.5, 0.5, 0.5,
-            0.5, 0.5, 0.5,
-            0.5, 0.5, -0.5,
-
-            -0.5, -0.5, -0.5,
-            0.5, -0.5, -0.5,
-            -0.5, -0.5, 0.5,
-            -0.5, -0.5, 0.5,
-            0.5, -0.5, -0.5,
-            0.5, -0.5, 0.5,
-
-            -0.5, -0.5, -0.5,
-            -0.5, -0.5, 0.5,
-            -0.5, 0.5, -0.5,
-            -0.5, -0.5, 0.5,
-            -0.5, 0.5, 0.5,
-            -0.5, 0.5, -0.5,
-
-            0.5, -0.5, -0.5,
-            0.5, 0.5, -0.5,
-            0.5, -0.5, 0.5,
-            0.5, -0.5, 0.5,
-            0.5, 0.5, -0.5,
-            0.5, 0.5, 0.5,
-
-        ]);
-    gl.bufferData(gl.ARRAY_BUFFER, positions, gl.STATIC_DRAW);
-}
-
-const setTexcoords = (gl: any) => {
-    gl.bufferData(
-        gl.ARRAY_BUFFER,
-        new Float32Array(
-            [
-                0, 0,
-                0, 1,
-                1, 0,
-                0, 1,
-                1, 1,
-                1, 0,
-
-                0, 0,
-                0, 1,
-                1, 0,
-                1, 0,
-                0, 1,
-                1, 1,
-
-                0, 0,
-                0, 1,
-                1, 0,
-                0, 1,
-                1, 1,
-                1, 0,
-
-                0, 0,
-                0, 1,
-                1, 0,
-                1, 0,
-                0, 1,
-                1, 1,
-
-                0, 0,
-                0, 1,
-                1, 0,
-                0, 1,
-                1, 1,
-                1, 0,
-
-                0, 0,
-                0, 1,
-                1, 0,
-                1, 0,
-                0, 1,
-                1, 1,
-
-            ]),
-        gl.STATIC_DRAW);
-}
